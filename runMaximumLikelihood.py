9d14ac3cb648d2ec90385c0fe67498113a5f35e8
Mike <mili7522@uni.sydney.edu.au>
2018-12-03 19:33:23 +1100

    Fix save name bug and added save for all minimisation repeats

--- runMaximumLikelihood.py
+++ runMaximumLikelihood.py
@@ -1,7 +1,6 @@
 import TTWMaximumLikelihood as TTWML
 import numpy as np
 import pandas as pd
-import scipy.stats
 import json
 import os
 import sys
@@ -12,7 +11,7 @@ import sys
 if len(sys.argv) > 1:
     model_number = int(sys.argv[1])
 else:
-    model_number = 1
+    model_number = 2
     # Model 1: Gravitational; 2: Log; 3: c_w = 0; 4: c_l = 0 (gravitational); 5: c_l = 0 (log)
 
 if len(sys.argv) > 2:
@@ -23,9 +22,12 @@ else:
 if len(sys.argv) > 3:
     year = int(sys.argv[3])
 else:
-    year = 2016
+    year = 2011
 
-trial_name = 'Sydney{}{}'.format(model_number, year)
+if bootstrap_id is None:
+    trial_name = 'Sydney{}_{}-Data'.format(year, model_number)
+else:
+    trial_name = 'Sydney{}_{}-{:02d}'.format(year, model_number, bootstrap_id)
 savePath = 'Results/{}'.format(trial_name)
 os.makedirs(savePath, exist_ok=True)
 
@@ -33,7 +35,7 @@ os.makedirs(savePath, exist_ok=True)
 
 ### Load Sydney Geography
 if bootstrap_id is None:
-    TTW = pd.read_table('Data/{}_TTW.csv'.format(year), sep = ',', index_col= 0)  # TODO: Fix 2011 import
+    TTW = pd.read_table('Data/{}_TTW.csv'.format(year), sep = ',', index_col= 0)
 else:
     TTW = pd.read_table('Data/BootstrapTTW{}/BootstrapTTW{:02d}.csv'.format(year, bootstrap_id), sep = ',', index_col= 0)
 
@@ -46,9 +48,7 @@ TTW = TTW.values
 distances = pd.read_csv("Data/{}_SA2-DriveTimes-OSRM.csv".format(year), header = None)
 distances = ((distances + distances.transpose()) / 2 / 60).values + 5
 
-
 a = TTWML.TTWML(distances, TTW, logForm = True if model_number in [2, 5] else False)
-
 
 suburbs = len(TTW)
 workDistribution = a.workDistribution
@@ -56,9 +56,8 @@ totalHouseholds = a.totalHouseholds
 
 
 
-
 ###  Obtain parameters using maximum likelihood
-fixed_params = {'c_w': 0,
+other_params = {'c_w': 0,
                 'c_l': 0,
                 'alpha_w': 1,
                 'beta': 1,
@@ -72,22 +71,23 @@ bounds = {'c_w': (0, None),
 
 resultList = []
 loglikelihoodList = []
-for i in range(30):
+for i in range(10):
     initParams = {'c_w': 5, 'c_l': 1, 'alpha_w': 5}
     if model_number == 3:
         del initParams['c_w']
     if model_number in [4,5]:
         del initParams['c_l']
     print(i)
-    finalParams, loglikelihood = a.maximiseLikelihood(initParams, bounds, trials = 2000, fixed_params = fixed_params, TTWArray = TTW)
-    resultList.append(finalParams)
-    loglikelihoodList.append(loglikelihood)
-    print('loglikelihoodList', loglikelihoodList)
+    finalParams, loglikelihood = a.maximiseLikelihood(initParams, bounds, trials = 100, fixed_params = other_params, TTWArray = TTW)
+    if loglikelihood is not None:
+        resultList.append(finalParams)
+        loglikelihoodList.append(loglikelihood)
+        print('loglikelihoodList', loglikelihoodList)
+        sys.stdout.flush()
     
 r = pd.DataFrame(resultList)
-# sns.pairplot(r, markers="x", plot_kws = {'alpha': 0.1})
-# plt.savefig(savePath + 'Generated-Recovered4Params{}.png'.format(trial), dpi = 250, format = 'png', bbox_inches = 'tight')
-# plt.close()
+r['NegLogLikelihood'] = loglikelihoodList
+r.to_csv(os.path.join(savePath,'MinLLRepetitions{}.csv'.format(trial_name)), index = False)
 
 print('Parameter Mean:', r.mean())
 print('Parameter Median:', r.median())
@@ -96,57 +96,24 @@ print('Likelihood Mean:', np.mean(loglikelihoodList))
 ###
 print('Minimum likelihood', np.min(loglikelihoodList))
 argmin = np.argmin(loglikelihoodList)
-r = resultList[argmin]
+r_min = resultList[argmin]
 
-combined_params = {**fixed_params, **r}
-
-P = a.probabilityFromParameters(combined_params)
+# Get P(i,j)
+combined_params = {**other_params, **r_min}
+P = a.probabilityFromParameters(combined_params, printParams = False)
 
 # Save probability matrix
-np.savetxt(savePath + 'ProbabilityMatrix{}.csv'.format(trial_name), P, delimiter = ',')
+np.savetxt(os.path.join(savePath,'ProbabilityMatrix{}.csv'.format(trial_name)), P, delimiter = ',')
 # Save the parameters
 combined_params['local_energy'] = list(combined_params['local_energy'])
-with open(savePath + 'Parameters{}.txt'.format(trial_name), 'w') as outfile:
+with open(os.path.join(savePath,'Parameters{}.txt'.format(trial_name)), 'w') as outfile:
     json.dump(combined_params, outfile)
 combined_params['local_energy'] = np.array(combined_params['local_energy'])
 
-# plotTTW(np.round(P * np.sum(TTW)), saveName = savePath + 'RecoveredTTW{}.png'.format(trial_name))
-# plotTTW(np.log(np.round(P * np.sum(TTW))), saveName = savePath + 'RecoveredTTWLog{}.png'.format(trial_name))
+
 
 p = P.ravel()
 q = TTW.ravel() / totalHouseholds
 print('---')
-print('Hellinger Distance:', TTWML.hellingerDistance(q, p))
-m = (p + q) / 2
-js = (scipy.stats.entropy(p, m) + scipy.stats.entropy(q, m)) / 2
-print('Jensen-Shannon Divergence:', js)
-
-### Generate Sydney data using maximum likelihood parameters
-
-# entropyHH = []
-# entropyTTW = []
-# averageTimeToWork = []
-# for _ in range(1000):
-#     mllArray = a.arrayFromProbability(P, households = totalHouseholds, minimiseRandomness = False)
-#     timeToWork = np.sum(mllArray * distances) / totalHouseholds
-#     averageTimeToWork.append(timeToWork)
-#     entropyTTW.append(scipy.stats.entropy(mllArray.ravel()))
-#     entropyHH.append(scipy.stats.entropy(mllArray.sum(axis = 1)))
-
-# df = pd.DataFrame([entropyHH, entropyTTW, averageTimeToWork])
-# df = df.transpose()
-# df.columns = ['EntropyHH', 'EntropyTTW', 'AverageTimeToWork']
-# df.to_csv(savePath + 'MeasuresFromGeneratedHouseholds{}.csv'.format(trial), index = None)
-
-
-### Comparison with null model
-
-# a.randomiseEdges()
-# TTWRand = a.TTWArray
-
-# q = TTWRand.ravel() / totalHouseholds
-# print('---')
-# print('Hellinger Distance - With Null:', a.hellingerDistance(q, p))
-# m = (p + q) / 2
-# js = (scipy.stats.entropy(p, m) + scipy.stats.entropy(q, m)) / 2
-# print('Jensen-Shannon Divergence - With Null:', js)
+print('Hellinger Distance:', TTWML.hellingerDistance(p,q))
+print('Jensen-Shannon Divergence:', TTWML.jsDivergence(p,q))
